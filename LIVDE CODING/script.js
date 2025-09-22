86f73a8f647979c69cefdc036044c53b726eb436
SOUH4IL-afk <souhailkoutaya78@gmail.com>
2025-09-22 11:22:25 +0200

    Add files via upload

---
+++ LIVDE CODING/script.js
@@ -0,0 +1,23 @@
+const codesCompetences = ["C1", "C3", "C2", "C1", "C8", "C1", "C3", "C2"];
+
+// تعريف مصفوفة تحتوي على codes de compétences.
+
+const codeRecherche = prompt("Quel code de compétence voulez-vous compter ?");  
+
+//إدخال الرمز الذي يريد حساب عدد مرات تكراره
+
+let compteur = 0;
+
+//تخزين عدد التكرارات
+
+for (let code of codesCompetences) {
+  if (code === codeRecherche) {
+    compteur++;
+  }
+}
+
+// حلقة تمر على جميع عناصر المصفوفة إذا كان العنصر يطابق الرمز المُدخل، يتم زيادة العدّاد بمقدار 1
+
+console.log(`Le code "${codeRecherche}" apparaît ${compteur} fois.`);
+ 
+// يتم طباعة النتيجة في console
